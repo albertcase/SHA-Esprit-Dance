f2ea218826908601053f706e13baebc8605f4378
Demon Zhang <demon.zhang@samesamechina.com>
2016-06-02 15:50:12 +0800

    no message

--- bundle/WechatBundle/WebServiceController.php
+++ bundle/WechatBundle/WebServiceController.php
@@ -108,6 +108,7 @@ class WebServiceController extends Controller {
 
 	public function jssdkConfig($url) {
 		$RedisAPI = new \Lib\RedisAPI();
+		var_dump($RedisAPI);exit;
 		$jsapi_ticket = $RedisAPI->getJSApiTicket();
 		$wechatJSSDKAPI = new \Lib\JSSDKAPI();
 		return $wechatJSSDKAPI->getJSSDKConfig(APPID, $jsapi_ticket, $url);
