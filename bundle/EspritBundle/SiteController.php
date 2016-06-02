088a95df46593b6ceddb83d42de64ee50e4b2792
Demon Zhang <demon.zhang@samesamechina.com>
2016-06-02 15:39:48 +0800

    no message

--- bundle/EspritBundle/SiteController.php
+++ bundle/EspritBundle/SiteController.php
@@ -8,17 +8,7 @@ class SiteController extends Controller {
 
 	public function indexAction($id) {
 		$DatabaseAPI = new \Lib\DatabaseAPI();
-		$video = $DatabaseAPI->findVideoById($id);
-		$file = $DatabaseAPI->findFileByFid($video->fid);
-
-		$this->render('index', array('url' => $file->filename, 'vid' => $video->vid , 'ismy' => 1));
-		exit;
-	}
-
-	public function videoAction($id) {
-		$DatabaseAPI = new \Lib\DatabaseAPI();
 		$user = $DatabaseAPI->userLoad();
-		var_dump($user);exit;
 		if (!$user) {
 			$parameterAry = $_GET;
 			if(count($parameterAry)>0)
@@ -29,9 +19,15 @@ class SiteController extends Controller {
 			$this->redirect("http://espritdance.samesamechina.com/wechat/ws/oauth2?redirect_uri=http://espritdance.samesamechina.com/callback&scope=snsapi_base");
 			exit;
 		}
-		$DatabaseAPI = new \Lib\DatabaseAPI();
 		$video = $DatabaseAPI->findVideoById($id);
 		$file = $DatabaseAPI->findFileByFid($video->fid);
+
+		$this->render('index', array('url' => $file->filename, 'vid' => $video->vid , 'ismy' => 1));
+		exit;
+	}
+
+	public function videoAction($id) {
+
 		$this->render('video', array('url' => $file->filename, 'vid' => $video->vid));
 		exit;
 	}
