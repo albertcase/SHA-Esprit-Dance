1086160033b7e9ddbc03d706d9454a59ffad9d00
Demon Zhang <demon.zhang@samesamechina.com>
2016-06-16 16:49:12 +0800

    no message

--- config/router.php
+++ config/router.php
@@ -9,6 +9,7 @@ $routers['/wechat/ws/jssdk/config/webservice'] = array('WechatBundle\WebService'
 $routers['/wechat/ws/jssdk/config/js'] = array('WechatBundle\WebService', 'jssdkConfigJs');
 $routers['/video/%'] = array('EspritBundle\Site', 'index');
 $routers['/callback'] = array('EspritBundle\Site', 'callback');
+$routers['/callback2'] = array('EspritBundle\Site', 'callback2');
 $routers['/api/submit'] = array('EspritBundle\Api', 'submit');
 $routers['/api/ballot'] = array('EspritBundle\Api', 'ballot');
 $routers['/test/%'] = array('EspritBundle\Site', 'test');
